fcf4f8bb51d6bca70c2443b5732a9596278adf3e
Marius Muntean <marius.muntean@live.com>
2018-11-24 09:54:51 +0100

    Painted a few circles on a larger circle

--- WheelSpinner/Views/SpinnerWheel.cs
+++ WheelSpinner/Views/SpinnerWheel.cs
@@ -1,3 +1,4 @@
+using System;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
@@ -20,14 +21,35 @@ namespace WheelSpinner.Views
             SKCanvas canvas = surface.Canvas;
 
             canvas.Clear();
-            
-            
+
+
             SKPaint paint = new SKPaint
             {
                 Style = SKPaintStyle.Stroke,
                 Color = Color.Red.ToSKColor(),
                 StrokeWidth = 25
             };
+
+            var centerPoint = new SKPoint(info.Width / 2f, info.Height / 2f);
+            var radius = Math.Min(info.Width / 2f, info.Height / 2f) * 0.8f;
+
+            // Draw large circle
+            canvas.DrawCircle(centerPoint, radius, paint);
+
+            // Draw little circles
+            var angle = 0.0f;
+            var degreeToRadianFactor = Math.PI / 180;
+            var littleCircleRadius = radius * 0.1f;
+            for (int i = 0; i < 13; i++)
+            {
+                var littleCircleCenter = new SKPoint(
+                                             (float) (radius * Math.Cos(angle * degreeToRadianFactor)),
+                                             (float) (radius * Math.Sin(angle * degreeToRadianFactor))
+                                         ) + centerPoint;
+
+                canvas.DrawCircle(littleCircleCenter, littleCircleRadius, paint);
+                angle += 30;
+            }
         }
     }
 }
