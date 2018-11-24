b2e4e2a631a15b295cb63f640cc9cbc63d628441
Marius Muntean <marius.muntean@live.com>
2018-11-24 09:44:22 +0100

    Added SkiaSharp

---
+++ WheelSpinner/Views/SpinnerWheel.cs
@@ -0,0 +1,33 @@
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace WheelSpinner.Views
+{
+    public class SpinnerWheel : ContentView
+    {
+        public SpinnerWheel()
+        {
+            SKCanvasView canvasView = new SKCanvasView();
+            canvasView.PaintSurface += OnCanvasViewPaintSurface;
+            Content = canvasView;
+        }
+
+        private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+            
+            
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Red.ToSKColor(),
+                StrokeWidth = 25
+            };
+        }
+    }
+}
