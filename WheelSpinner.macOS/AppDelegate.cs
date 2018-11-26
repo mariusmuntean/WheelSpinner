02015dd52d637dab513133690cf712f22e9a65db
Marius <marius.muntean@live.com>
2018-11-26 22:20:28 +0100

    stuff

---
+++ WheelSpinner.macOS/AppDelegate.cs
@@ -0,0 +1,35 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace WheelSpinner.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow _window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 400, 768, 1024);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            _window.Title = "WheelSpinner";
+            _window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow
+        {
+            get => _window;
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+    }
+}
